24e5234733dd35e9195e633036e8af3915f86a7a
Zach White <zechnwhite@gmail.com>
2019-11-23 18:55:51 -0800

    adsadsf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express= require('express');
 const app = express();
 const path = require('path');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static( 'client/build' ));

c76c50d6ea98296ec5198f25541480715e640655
Zach White <zechnwhite@gmail.com>
2019-11-26 20:12:49 -0800

    adds new ps image for pct

--- client/src/components/PCT/TrailImage/TrailImage.js
+++ client/src/components/PCT/TrailImage/TrailImage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import pctmap from '../../../assets/img/pctmap.jpg';
+import pctmap from '../../../assets/img/pctmap.png';
 import styles from './TrailImage.module.css'
 
 export default function TrailImage(){

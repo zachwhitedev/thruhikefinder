e796fcaeb1651e92d45746f472a6e51de15738cf
Zach White <52550132+zachwhitedev@users.noreply.github.com>
2019-11-30 21:31:52 -0800

    pct content filled (#31)

--- client/src/components/PCT/MoreStats/MoreStats.js
+++ client/src/components/PCT/MoreStats/MoreStats.js
@@ -7,16 +7,36 @@ export default function MoreStats() {
       <h2>More Stats</h2>
       <table className={styles.myTable}>
         <tr>
-          <td>Elev. Change per Mile</td>
-          <td>184 ft.</td>
+          <td>Year Established</td>
+          <td>1968</td>
         </tr>
         <tr>
-          <td>Avg. Distance Between Resupplies</td>
-          <td>102 mi.</td>
+          <td>Approximate # Days to Complete</td>
+          <td>150 days</td>
+        </tr>
+        <tr>
+          <td>Avg. Elevation Gain per Mile</td>
+          <td>184 feet</td>
+        </tr>
+        <tr>
+          <td>Average Monthly Rainfall</td>
+          <td>1.15 inches</td>
         </tr>
         <tr>
           <td>Longest Stretch of No Water</td>
           <td>42 miles</td>
+        </tr>
+        <tr>
+          <td>Predicted # of Resupplies</td>
+          <td>26</td>
+        </tr>
+        <tr>
+          <td>Avg. Distance Between Resupplies</td>
+          <td>102 miles</td>
+        </tr>
+        <tr>
+          <td>Longest Resupply Distance</td>
+          <td>154 miles</td>
         </tr>
       </table>
     </div>   

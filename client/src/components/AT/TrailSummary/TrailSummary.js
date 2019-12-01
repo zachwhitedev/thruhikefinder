30bb28e7699b658965ca56f66ee45b54d738adc1
Peter White <peterrolandwhite@gmail.com>
2019-11-30 21:14:23 -0700

    nobo v sobo

--- client/src/components/AT/TrailSummary/TrailSummary.js
+++ client/src/components/AT/TrailSummary/TrailSummary.js
@@ -6,17 +6,13 @@ export default function TrailSummary(){
         <div>
         <h2>Summary</h2>
         <p>
-          The Pacific Crest Trail is a 2,652 mile continous footpath that runs
-          the entire length of the west coast of the United States. It starts at
-          the Mexican border in Southern California, and cross three states to
-          where it ends at the Canadian border in Washington. The trail is known
-          for its breath-taking views, varied terrain, and continuosly changing
-          climate and landscapes.
+          The Appalachian Trail. The Beast of the East. Winding between the summits of Springer Mountain, Georgia and Mount Katahdin, Maine, the AT is the quintessential American thru-hike. While it is the shortest of the three “triple-crown” trails, this should not be interpreted as a reflection of its difficulty - the AT boasts significantly more elevation change per mile than its counterparts. Hikers will generally walk fewer miles per day on the AT than they would on a more open, western trail. Add in bugs and frequent rain, and this trail is as formidable as they come.
         </p>
         <p>
-          In completing the trail, you will have crossed through the desert of
-          Southern California, the grandiose Sierra Nevada mountain range, and
-          the wilderness of the Cascades in Northern Washington.
+          The AT takes hikers through dense, green forests, over crags and summits where the foliage recedes to reveal seas of rolling mountains, blue and hazy in the distance. The trail is consistently well maintained and exceptionally easy to follow, although it has earned a reputation for often taking a fairly “direct” route to a summit.
+        </p>
+        <p>
+          Particularly notable is the extent to which the AT is a social experience. Of course there will always be a degree of solitude on any long hike, and the AT is far from being the only trail with a social component. But between the large number of hikers and the system of clearly delineated shelters and campsites that line the trail, community thrives on the AT.
         </p>
       </div>
     )

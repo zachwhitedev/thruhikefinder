c448a12b5b9264d66fa55ea5ee7928a7b7ac6ddc
Zach White <52550132+zachwhitedev@users.noreply.github.com>
2019-11-30 20:17:04 -0800

    nobo v sobo (#30)

--- client/src/components/LT/TrailSummary/TrailSummary.js
+++ client/src/components/LT/TrailSummary/TrailSummary.js
@@ -6,17 +6,13 @@ export default function TrailSummary(){
         <div>
         <h2>Summary</h2>
         <p>
-          The Pacific Crest Trail is a 2,652 mile continous footpath that runs
-          the entire length of the west coast of the United States. It starts at
-          the Mexican border in Southern California, and cross three states to
-          where it ends at the Canadian border in Washington. The trail is known
-          for its breath-taking views, varied terrain, and continuosly changing
-          climate and landscapes.
+          Traversing the ridge of Vermont’s Green Mountains from Massachusetts to Canada, the Long Trail is among New England’s most treasured landmarks. The LT is the oldest long-distance hiking trail in the country, and it served as a model for the Appalachian Trail, even down to the color of the blazes.
         </p>
         <p>
-          In completing the trail, you will have crossed through the desert of
-          Southern California, the grandiose Sierra Nevada mountain range, and
-          the wilderness of the Cascades in Northern Washington.
+          Mile-for-mile, the LT is among the most physically demanding hikes in the country. Steep climbs are frequent, not separated by long stretches of “cruise-trail” as they can be on longer hikes. The rewards, however, are worth a little sweat. Rocky summits, panoramas of Vermont’s sparsely-populated countryside, and deep forests define the LT’s scenery.
+        </p>
+        <p>
+          Few other trails provide such a complete thru-hiking experience inside of a month. Trail towns, shelters, border tags, and a vibrant hiker community make the LT feel like a distillation of many elements often found in much longer hikes. Although it shares history (and one hundred miles) with the AT, the LT stands proud on its own. Rustic, challenging, and accessible, the LT is a gem.
         </p>
       </div>
     )

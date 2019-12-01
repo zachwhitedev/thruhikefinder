e943d48790f4c5586c11ec68bbfc885b964220fc
Peter White <peterrolandwhite@gmail.com>
2019-11-30 23:26:06 -0700

    pct typos

--- client/src/components/PCT/NoboSobo/NoboSobo.js
+++ client/src/components/PCT/NoboSobo/NoboSobo.js
@@ -23,7 +23,7 @@ export default function NoboSobo() {
         </tr>
         <tr>
           <td>TIMETABLE</td>
-          <td><p>After completing the desert section in the Spring, many northbounders find themselves waiting around for the snow in the Sierra to melt, before entering them some time in June. Though the more adventurous might press on into the snow anyway, many will take 1-3 weeks off from the trail or “flip up” to complete sections of the trail North of the Sierra (often choosing lower elevation trail in Oregon) while they wait for better conditions.</p>
+          <td><p>After completing the desert section in the spring, many northbounders find themselves waiting around for the snow in the Sierra to melt, before entering them some time in June. Though the more adventurous might press on into the snow anyway, many will take 1-3 weeks off from the trail or “flip up” to complete sections of the trail north of the Sierra (often choosing lower elevation trail in Oregon) while they wait for better conditions.</p>
           <p>The Cascades in Northern Washington can receive hike-ending levels of snowfall as early as mid-September, so most aim to reach the Northern Terminus by October 1st.</p></td>
           <td><p>There is something of a time crunch to get through the Sierra Nevada before mid-October - the standard “deadline” most give themselves to beat the snow (though nature is fickle and the snow can arrive both sooner and much later, depending on the year). Even if you start your hike in late June, you cannot waste too much time if you are serious about hiking the entire trail in one go.</p>
           <p>However, once you reach South Kennedy Meadows, you can relax knowing that you can go as slow as you want for the final 700 miles of the trail (the desert section is hikable throughout the fall/winter months).</p></td>
@@ -35,33 +35,33 @@ export default function NoboSobo() {
         </tr>
         <tr>
           <td>FIRST MILES</td>
-          <td>The desert section in SoCal, being relatively flat, is a great “warm up” for Northbounders.</td>
-          <td>Starting in Northern Washington, you are thrown right into one of the most rugged and remote sections of the trail.</td>
+          <td>The desert section in SoCal, being relatively flat, is a great “warm up” for northbounders.</td>
+          <td>Starting in northern Washington, you are thrown right into one of the most rugged and remote sections of the trail.</td>
         </tr>
         <tr>
           <td>DESERT TEMPS</td>
-          <td>Desert temperatures will be hotter - but Nobos get to see the Spring desert flower bloom.</td>
+          <td>Desert temperatures will be hotter - but Nobos get to see the spring desert flower bloom.</td>
           <td>Desert temperatures are cooler, but no pretty flowers.</td>
         </tr>
         <tr>
           <td>SNOW</td>
           <td>Regardless of how late in June you enter the Sierra, there will likely be snow waiting for you atop the higher passes. Plan accordingly.</td>
-          <td>Starting in Northern Washington in late-June or July means southbounders get their own stretch of snow, right at the start. Ace axe and spikes in the Cascades can be necessities, depending on the snowpack level. Post-holing and long sections of wet trail are common occurrences.</td>
+          <td>Starting in Northern Washington in late-June or July means southbounders get their own stretch of snow, right at the start. An ice axe and spikes in the Cascades can be necessities, depending on the snowpack level. Post-holing and long sections of wet trail are common occurrences.</td>
         </tr>
         <tr>
           <td>SIERRA</td>
-          <td>In addition to the snow on the ground, the Spring snowmelt in the Sierra Nevada translates to mosquitoes and perilous river crossings during this large section of California trail - you will also be sharing the trail with many day-hikers during peak tourist season.</td>
+          <td>In addition to the snow on the ground, the spring snowmelt in the Sierra Nevada translates to mosquitoes and perilous river crossings during this large section of California trail - you will also be sharing the trail with many day-hikers during peak tourist season.</td>
           <td>For Sobos in the Sierra, there are no perilous river crossings and virtually no bugs whatsoever during this large section of the trail. In the fall, the Sierra will be much less crowded as well.</td>
         </tr>
         <tr>
           <td>BITING BUGS</td>
           <td>Nobos have it worse, as they essentially follow the hatching season. There are few bugs in the desert, but once they enter the Sierra, they get no relief until they reach Washington.</td>
-          <td>Sobos walk against the hatching season. Though the bugs can get bad in Southern Washington and Northern Oregon, once they cross into California, they might never feel another bite.</td>
+          <td>Sobos walk against the hatching season. Though the bugs can get bad in southern Washington and northern Oregon, once they cross into California, they might never feel another bite.</td>
         </tr>
         <tr>
           <td>Northern Terminus</td>
           <td>Northbound hikers do not have to do an “out and back” to touch the Northern Terminus, they simply hike straight to it. However, in order to enter Canada after reaching the Northern Terminus, you must obtain a <a href="https://www.pcta.org/discover-the-trail/permits/canada-pct-entry-permit/" target="_blank">PCT Canada Entry Permit.</a></td>
-          <td>Southbounders must begin their hike within the United States, as it is illegal to enter the United States from Manning Park, Canada. For this reason, most choose to begin their hike at Hart’s Pass, which is roughly 30 miles south of the Northern Terminus. They must hike the 30 miles to the Northern Terminus, and hike the 30 miles back to Hart’s Pass (there are no roads to hitch back, as it is too remote/mountainous).</td>
+          <td>Southbounders must begin their hike within the United States, as it is illegal to enter the United States from Manning Park, Canada. For this reason, most choose to begin their hike at Hart’s Pass, which is on the PCT and roughly 30 miles south of the Northern Terminus. They must hike the 30 miles to the Northern Terminus, then turnaround and hike the 30 miles back (there are no roads to hitch back, as it is too remote/mountainous).</td>
         </tr>
         <tr>
           <td>Trail Magic</td>

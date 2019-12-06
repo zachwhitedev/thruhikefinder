661925caca50ca2079d3c3205864a092ed44a082
Peter White <peterrolandwhite@gmail.com>
2019-12-05 21:34:27 -0700

    more work on AT

--- client/src/components/AT/GearRec/GearRec.js
+++ client/src/components/AT/GearRec/GearRec.js
@@ -1,24 +1,52 @@
-import React from 'react';
-import styles from './GearRec.module.css';
+import React from "react";
+import styles from "./GearRec.module.css";
 
 export default function GearRec() {
   const tentimage =
-    'https://images-na.ssl-images-amazon.com/images/I/41fc3313RVL._SL250_.jpg';
+    "https://images-na.ssl-images-amazon.com/images/I/41fc3313RVL._SL250_.jpg";
   const tentlink =
-    'https://www.amazon.com/gp/product/B07MTSM7ZL/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07MTSM7ZL&linkCode=as2&tag=nelsonshepher-20&linkId=8476d7d82c5b613b58dc9ccc9c47cde1';
+    "https://www.amazon.com/gp/product/B07MTSM7ZL/ref=as_li_tl?ie=UTF8&camp=1789&creative=9325&creativeASIN=B07MTSM7ZL&linkCode=as2&tag=nelsonshepher-20&linkId=8476d7d82c5b613b58dc9ccc9c47cde1";
 
   return (
     <div className={styles.gear}>
-      <h1>Gear Recommendation</h1>
-      <div className={styles.tentDiv}>
-        <h5>Nemo Hornet</h5>
-        <a href={tentlink}>
-          <img src={tentimage} alt='tent for hikers' />
-        </a>
+      <h2>Gear Recommendation</h2>
+      <div className={styles.saleItems}>
+        <div className={styles.tentDiv}>
+          <div className={styles.gearName}>The Nemo Hornet</div>
+          <a href={tentlink} target="_blank">
+            <img
+              id={styles.productImage}
+              src={tentimage}
+              alt="tent for hiker"
+            />
+          </a>
 
-        <a id={styles.tentButton} href={tentlink} target='_blank'>
-          Buy Tent
-        </a>
+          <a id={styles.tentButton} href={tentlink} target="_blank">
+            Shop Now
+          </a>
+        </div>
+        <div className={styles.p}>
+          <div className={styles.ourReview}>Our Review</div>
+          <p>
+            This is truly a “tent for all seasons" - cheaper than the super
+            expensive cottage brands, but still ultralight at just 1 lb 10 oz.
+          </p>
+          <p>
+            Extremely durable, reliable, and quick to set up - clutch for when
+            you’re exhausted and just want to dive into your sleeping bag.
+          </p>
+          <p>
+            Other ultralight tents from companies like Zpacks (and others that
+            use DCF as their fabric) trap condensation and drip water on you
+            while you sleep - this one won’t.
+          </p>
+          <p>
+            The PCT has a wide variety of campsites, from the outskirts of the
+            Mojave Desert to the High Sierra - with this tent in your pack you
+            are always prepared.
+          </p>
+          <p>We recommend the 2 person version for those on the taller side.</p>
+        </div>
       </div>
     </div>
   );

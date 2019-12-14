1a2f2b00f94cd2cf2f861e7151012086b2bf014a
Peter White <peterrolandwhite@gmail.com>
2019-12-13 18:10:49 -0800

    rain jacket

--- client/src/components/AT/KeyFacts/KeyFacts.js
+++ client/src/components/AT/KeyFacts/KeyFacts.js
@@ -12,7 +12,39 @@ export default function KeyFacts() {
         </tr>
         <tr>
           <td>Highest Point</td>
-          <td>Forester Pass - 13,153 feet</td>
+          <td>Clingman's Dome (NC) - 6,643 feet</td>
+        </tr>
+        <tr>
+          <td>Year Established</td>
+          <td>1923</td>
+        </tr>
+        <tr>
+          <td>Approximate # Days to Complete</td>
+          <td>165 days</td>
+        </tr>
+        <tr>
+          <td>Avg. Elevation Gain per Mile</td>
+          <td>212 feet</td>
+        </tr>
+        <tr>
+          <td>Average Monthly Rainfall</td>
+          <td>N/A</td>
+        </tr>
+        <tr>
+          <td>Longest Stretch of No Water</td>
+          <td>N/A</td>
+        </tr>
+        <tr>
+          <td>Predicted # of Resupplies</td>
+          <td>69</td>
+        </tr>
+        <tr>
+          <td>Avg. Distance Between Resupplies</td>
+          <td>32 miles</td>
+        </tr>
+        <tr>
+          <td>Longest Resupply Distance</td>
+          <td>100 miles</td>
         </tr>
       </table>
     </div>

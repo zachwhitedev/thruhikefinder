4ee673c5ee2e6f84e29a4e8d8c54364d885045b7
Zach White <zechnwhite@gmail.com>
2019-12-15 13:35:26 -0800

    yep

--- client/src/blog/posts/Post1ATMurders/Post1ATMurders.js
+++ client/src/blog/posts/Post1ATMurders/Post1ATMurders.js
@@ -34,6 +34,20 @@ export default function Post1ATghosts() {
           trains. Additional versions of the speech appeared in newspapers of
           the era, feeding modern-day confusion about the authoritative text.
         </p>
+        <p>
+          There are five known copies of the speech in Lincoln's handwriting,
+          each with a slightly different text, and named for the people who
+          first received them: Nicolay, Hay, Everett, Bancroft and Bliss. Two
+          copies apparently were written before delivering the speech, one of
+          which probably was the reading copy. The remaining ones were produced
+          months later for soldier benefit events. Despite widely-circulated
+          stories to the contrary, the president did not dash off a copy aboard
+          a train to Gettysburg. Lincoln carefully prepared his major speeches
+          in advance; his steady, even script in every manuscript is consistent
+          with a firm writing surface, not the notoriously bumpy Civil War-era
+          trains. Additional versions of the speech appeared in newspapers of
+          the era, feeding modern-day confusion about the authoritative text.
+        </p>
       </div>
     </div>
   );

74dcb79d71c5b0fd963f31d6a62ab3216c87b3bc
Zach White <zechnwhite@gmail.com>
2019-12-20 19:21:14 -0800

    all thru hikes table sorting

--- client/src/components/TrailTables/AllTrailsTable/AllTrailsTable.js
+++ client/src/components/TrailTables/AllTrailsTable/AllTrailsTable.js
@@ -7,11 +7,11 @@ export default class AllTrailsTable extends React.Component {
   state = {
     sortType: {
       up: {
-        class: 'sort-up',
+        class: 'sort',
         fn: (a, b) => a.distance - b.distance
       },
       down: {
-        class: 'sort-down',
+        class: 'sort',
         fn: (a, b) => b.distance - a.distance
       },
       default: {
@@ -29,7 +29,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 150,
         elevationPerMile: 184,
         region: 'West',
-        highPoint: '13,153',
+        highPoint: 13153,
         rainfall: 1.15,
         predictedResupplies: 27,
         avgResupplies: 98,
@@ -43,7 +43,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 170,
         elevationPerMile: 172,
         region: 'Southwest/West',
-        highPoint: '14,278',
+        highPoint: 14278,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A',
@@ -57,7 +57,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 165,
         elevationPerMile: 212,
         region: 'East',
-        highPoint: '6,643',
+        highPoint: 6643,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A',
@@ -71,7 +71,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 20,
         elevationPerMile: 239,
         region: 'Northeast',
-        highPoint: '4,395',
+        highPoint: 4395,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A',
@@ -85,7 +85,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 20,
         elevationPerMile: 221,
         region: 'West',
-        highPoint: '14,505',
+        highPoint: 14505,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A',
@@ -99,7 +99,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 40,
         elevationPerMile: 'N/A',
         region: 'Southwest',
-        highPoint: '9,148',
+        highPoint: 9148,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A'
@@ -112,7 +112,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 30,
         elevationPerMile: 185,
         region: 'West',
-        highPoint: '13,271',
+        highPoint: 13271,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A'
@@ -125,7 +125,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 60,
         elevationPerMile: 171,
         region: 'West',
-        highPoint: '7,800',
+        highPoint: 7800,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A'
@@ -138,7 +138,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 45,
         elevationPerMile: 125,
         region: 'Southwest',
-        highPoint: '11,419',
+        highPoint: 11419,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A'
@@ -177,7 +177,7 @@ export default class AllTrailsTable extends React.Component {
         daysToFinish: 25,
         elevationPerMile: 'N/A',
         region: 'Midwest',
-        highPoint: '1,772',
+        highPoint: 1772,
         rainfall: 'N/A',
         predictedResupplies: 'N/A',
         avgResupplies: 'N/A'
@@ -187,94 +187,255 @@ export default class AllTrailsTable extends React.Component {
 
   // method called every time the sort button is clicked
   // it will change the currentSort value to the next one
-  onSortChange = () => {
-    this.setState({
-      sortType: {
-        up: {
-          class: 'sort-up',
-          fn: (a, b) => a.distance - b.distance
-        },
-        down: {
-          class: 'sort-down',
-          fn: (a, b) => b.distance - a.distance
-        },
-        default: {
-          class: 'sort',
-          fn: (a, b) => a.distance - b.distance
+  onSortChange = (attribute) => {
+    if(attribute == 'distance'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.distance - b.distance
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.distance - a.distance
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.distance - b.distance
+          }
         }
-      }
-    });
-
-    const { currentSort } = this.state;
-    let nextSort;
-
-    if (currentSort === 'down') nextSort = 'up';
-    else nextSort = 'down';
-
-    this.setState({
-      currentSort: nextSort
-    });
-  };
-
-  onSortChangeCock = () => {
-    this.setState({
-      sortType: {
-        up: {
-          class: 'sort-down',
-          fn: (a, b) => a.waterSources - b.waterSources
-        },
-        down: {
-          class: 'sort-up',
-          fn: (a, b) => b.waterSources - a.waterSources
-        },
-        default: {
-          class: 'sort-down',
-          fn: (a, b) => a.waterSources - b.waterSources
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'totalstates'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.totalstates - b.totalstates
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.totalstates - a.totalstates
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.totalstates - b.totalstates
+          }
         }
-      }
-    });
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'year'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.year - b.year
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.year - a.year
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.year - b.year
+          }
+        }
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'elevationPerMile'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.elevationPerMile - b.elevationPerMile
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.elevationPerMile - a.elevationPerMile
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.elevationPerMile - b.elevationPerMile
+          }
+        }
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'highPoint'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.highPoint - b.highPoint
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.highPoint - a.highPoint
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.highPoint - b.highPoint
+          }
+        }
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'daysToFinish'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.daysToFinish - b.daysToFinish
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.daysToFinish - a.daysToFinish
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.daysToFinish - b.daysToFinish
+          }
+        }
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'rainfall'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.rainfall - b.rainfall
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.rainfall - a.rainfall
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.rainfall - b.rainfall
+          }
+        }
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'predictedResupplies'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.predictedResupplies - b.predictedResupplies
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.predictedResupplies - a.predictedResupplies
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.predictedResupplies - b.predictedResupplies
+          }
+        }
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
+    if(attribute == 'avgResupplies'){
+      this.setState({
+        sortType: {
+          up: {
+            class: 'sort',
+            fn: (a, b) => a.avgResupplies - b.avgResupplies
+          },
+          down: {
+            class: 'sort',
+            fn: (a, b) => b.avgResupplies - a.avgResupplies
+          },
+          default: {
+            class: 'sort',
+            fn: (a, b) => a.avgResupplies - b.avgResupplies
+          }
+        }
+      });
+      const { currentSort } = this.state;
+      let nextSort;
+  
+      if (currentSort === 'down') nextSort = 'up';
+      else nextSort = 'down';
+  
+      this.setState({
+        currentSort: nextSort
+      });
+    }
 
-    const { currentSort } = this.state;
-    let nextSort;
-
-    if (currentSort === 'down') nextSort = 'up';
-    else nextSort = 'down';
-
-    this.setState({
-      currentSort: nextSort
-    });
   };
 
   render() {
     const { currentSort, sortType } = this.state;
-    const sortTypeNetWorth = {
-      up: {
-        class: 'sort-up',
-        fn: (a, b) => a.distance - b.distance
-      },
-      down: {
-        class: 'sort-down',
-        fn: (a, b) => b.distance - a.distance
-      },
-      default: {
-        class: 'sort-down',
-        fn: (a, b) => a.distance - b.distance
-      }
-    };
-    const sortTypeCock = {
-      up: {
-        class: 'sort-up',
-        fn: (a, b) => a.waterSources - b.waterSources
-      },
-      down: {
-        class: 'sort-down',
-        fn: (a, b) => b.waterSources - a.waterSources
-      },
-      default: {
-        class: 'sort-down',
-        fn: (a, b) => a.waterSources - b.waterSources
-      }
-    };
 
     return (
       <div className={styles.compWrapper}>
@@ -285,26 +446,98 @@ export default class AllTrailsTable extends React.Component {
               <thead>
                 <tr className={styles.allTrailsTH}>
                   <th>Trail Name</th>
-                  <th>Year Established</th>
+                  <th>Year Established
+                  <button
+                      onClick={() => this.onSortChange('year')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
                   <th>Region</th>
                   <th>
                     Distance (mi)
                     <button
-                      onClick={this.onSortChange}
+                      onClick={() => this.onSortChange('distance')}
                       aria-label='sort by distance'
                     >
                       <i
-                        className={`fas fa-${sortTypeNetWorth[currentSort].class}`}
+                        className={`fas fa-${sortType[currentSort].class}`}
                       />
                     </button>
                   </th>
-                  <th># States Entered</th>
-                  <th>~ Days to Finish</th>
-                  <th>Elev. Gain/Mile (ft)</th>
-                  <th>Highest Point (ft)</th>
-                  <th>Avg. Rainfall (in)</th>
-                  <th>Predicted # Resupplies</th>
-                  <th>Avg. Resupply Distance (mi)</th>
+                  <th># States Entered
+                  <button
+                      onClick={() => this.onSortChange('totalstates')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
+                  <th>~ Days to Finish
+                  <button
+                      onClick={() => this.onSortChange('daysToFinish')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
+                  <th>Elev. Gain/Mile (ft)
+                  <button
+                      onClick={() => this.onSortChange('elevationPerMile')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
+                  <th>Highest Point (ft)
+                  <button
+                      onClick={() => this.onSortChange('highPoint')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
+                  <th>Avg. Rainfall (in)
+                  <button
+                      onClick={() => this.onSortChange('rainfall')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
+                  <th>Predicted # Resupplies
+                  <button
+                      onClick={() => this.onSortChange('predictedResupplies')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
+                  <th>Avg. Resupply Distance (mi)
+                  <button
+                      onClick={() => this.onSortChange('avgResupplies')}
+                      aria-label='sort by distance'
+                    >
+                      <i
+                        className={`fas fa-${sortType[currentSort].class}`}
+                      />
+                    </button>
+                  </th>
                 </tr>
               </thead>
               <tbody>
